7100a21618858f6df95cd1d7a2f4d4eb87ce92c2
Giacomo Ritucci <giacomo.ritucci@email.it>
2011-01-30 23:17:51 +0100

    analyst: module stub

--- static/js/analyst.js
+++ static/js/analyst.js
@@ -1,6 +1,32 @@
+(function (global, io)
+{
+	var init_done = false,
+		self = {},
+		socket;
+
+
+	self.init = function ()
+	{
+		if (!init_done) {
+			socket = new io.Socket();
+			socket.connect();
+			socket.on("message", self.onMessage);
+		}
+	};
+
+
+	self.onMessage = function (msg)
+	{
+		console.log(msg);
+	};
+
+
+	self.init();
+
+	global.INSTANT = self;
+
+}(window, io));
+
 (function ()
 {
-	var socket = new io.Socket();
-	socket.connect();
-	socket.on("message", console.log);
 }());
