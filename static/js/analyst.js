5e960d2f46e1f734909df41bb92e8eb82e9be72b
Giacomo Ritucci <giacomo.ritucci@email.it>
2011-02-01 01:02:42 +0100

    analyst: onMessage -> console.log

--- static/js/analyst.js
+++ static/js/analyst.js
@@ -2,8 +2,9 @@
 {
 	var init_done = false,
 		self = {},
+		paper_width = 500,
+		paper_height = 500,
 		socket;
-
 
 	self.init = function ()
 	{
@@ -11,13 +12,21 @@
 			socket = new io.Socket();
 			socket.connect();
 			socket.on("message", self.onMessage);
+
+			self.paper = Raphael("paper", paper_width, paper_height);
+			self.pages = {};
 		}
 	};
 
 
 	self.onMessage = function (msg)
 	{
-		console.log(msg);
+		var name = msg.pageName;
+		if (!self.pages[name]) {
+			self.pages[name] = 0;
+		}
+		self.pages[name]++;
+		console.log(self.pages);
 	};
 
 
